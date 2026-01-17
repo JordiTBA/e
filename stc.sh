f2e5411e070a4b813581ada664863835b0e9e756
jason christofer handika <113273341+JordiTBA@users.noreply.github.com>
2026-01-17 18:20:34 +0700

    Update stc.sh

--- stc.sh
+++ stc.sh
@@ -5,13 +5,13 @@ while true; do
     TIMESTAMP=$(date '+%H:%M:%S')
     
     # Cek Roblox (tetap pakai full path buat dumpsys biar aman)
-    if "$SU_CMD" -c "/system/bin/dumpsys window windows" | grep -i 'mCurrentFocus' | grep -q 'com.roblox.client'; then
+    if "$SU_CMD" -c "/system/bin/dumpsys window windows" | grep -i 'mCurrentFocus' | grep -q 'com.asepv2/mobu'; then
         echo "[$TIMESTAMP] OK: Roblox sedang dimainkan."
     else
         echo "[$TIMESTAMP] ACTION: Roblox tidak di layar. Melakukan auto-join..."
         
         # Kill app
-        "$SU_CMD" -c "/system/bin/am force-stop com.roblox.client"
+        "$SU_CMD" -c "/system/bin/am force-stop com.asepv2/mobu"
         
         # Start app (pastikan satu baris)
         "$SU_CMD" -c "/system/bin/am start --user 0 -a android.intent.action.VIEW -d 'roblox://placeId=121864768012064&linkCode=29945061429931940452490641554963'"
