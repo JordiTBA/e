0cacfa9f78ac0696f479573656a4c2d2deea0da1
jason christofer handika <113273341+JordiTBA@users.noreply.github.com>
2026-01-17 18:14:22 +0700

    Update stc.sh

--- stc.sh
+++ stc.sh
@@ -1,13 +1,21 @@
+# Cari lokasi su otomatis
+SU_CMD=$(which su)
 
 while true; do
     TIMESTAMP=$(date '+%H:%M:%S')
     
-    if su -c "dumpsys window windows | grep -i 'mCurrentFocus' | grep -q 'com.roblox.client'"; then
+    # Cek Roblox (tetap pakai full path buat dumpsys biar aman)
+    if "$SU_CMD" -c "/system/bin/dumpsys window windows" | grep -i 'mCurrentFocus' | grep -q 'com.roblox.client'; then
         echo "[$TIMESTAMP] OK: Roblox sedang dimainkan."
     else
         echo "[$TIMESTAMP] ACTION: Roblox tidak di layar. Melakukan auto-join..."
-        su -c "am force-stop com.roblox.client" > /dev/null 2>&1
-        su -c "am start --user 0 -a android.intent.action.VIEW -d 'roblox://placeId=121864768012064&linkCode=29945061429931940452490641554963'" > /dev/null 2>&1
+        
+        # Kill app
+        "$SU_CMD" -c "/system/bin/am force-stop com.roblox.client"
+        
+        # Start app (pastikan satu baris)
+        "$SU_CMD" -c "/system/bin/am start --user 0 -a android.intent.action.VIEW -d 'roblox://placeId=121864768012064&linkCode=29945061429931940452490641554963'"
+        
         sleep 20
     fi
     sleep 5
